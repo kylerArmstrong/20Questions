9574f47da5ce22ce53eaa9fbd1c00a7dc154b3f8
kylerArmstrong <143137470+kylerArmstrong@users.noreply.github.com>
2025-01-27 13:16:32 -0600

    obj to str pt2

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -16,11 +16,11 @@ public class QuestionsGame {
 		public QuestionNode left;
 		public QuestionNode right;
 
-		public QuestionNode(Object d) {
+		public QuestionNode(String d) {
 			this(d, null, null);
 		}
 
-		public QuestionNode(Object d, QuestionNode l, QuestionNode r) {
+		public QuestionNode(String d, QuestionNode l, QuestionNode r) {
 			data = d;
 			left = l;
 			right = r;
@@ -28,7 +28,7 @@ public class QuestionsGame {
 	}
 
 	public QuestionsGame(String object) {
-		
+		overallNode = new QuestionNode(object);
 	}
 
 	public QuestionsGame(Scanner input) {
