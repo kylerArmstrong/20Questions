0f66847edb3ab3f7ca9f42baa8e1aae5f90a7b7a
SeasideWhisper <benjaminhouseholder0@gmail.com>
2025-01-27 12:51:54 -0600

    the fact that its permanant

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -1,3 +1,6 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
 // This is a starter file for QuestionsGame.
 //
 // You should delete this comment and replace it with your class
@@ -8,5 +11,21 @@ public class QuestionsGame {
 
     private static class QuestionNode {
         // Your code here
+    }
+    
+    public QuestionsGame(String object) {
+    	
+    }
+    
+    public QuestionsGame(Scanner input) {
+    	
+    }
+    
+    public void saveQeustions(PrintStream output) {
+    	
+    }
+    
+    public void play() {
+    	
     }
 }
